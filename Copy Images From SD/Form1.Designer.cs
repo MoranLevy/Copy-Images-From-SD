aa27fec39f07ad7326dcae699ef02d359b5c8bad
MoranLevy <36726801+MoranLevy@users.noreply.github.com>
2018-07-05 20:12:21 +0300

    Add copy only types support

--- Copy Images From SD/Form1.Designer.cs
+++ Copy Images From SD/Form1.Designer.cs
@@ -28,12 +28,223 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBoxSrc = new System.Windows.Forms.TextBox();
+            this.textBoxDest = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonBrowse1 = new System.Windows.Forms.Button();
+            this.buttonCopy = new System.Windows.Forms.Button();
+            this.buttonBrowse2 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.checkBoxJPG = new System.Windows.Forms.CheckBox();
+            this.checkBoxRaw = new System.Windows.Forms.CheckBox();
+            this.checkBoxVideo = new System.Windows.Forms.CheckBox();
+            this.statusStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxSrc
+            // 
+            this.textBoxSrc.Location = new System.Drawing.Point(101, 23);
+            this.textBoxSrc.Name = "textBoxSrc";
+            this.textBoxSrc.Size = new System.Drawing.Size(187, 20);
+            this.textBoxSrc.TabIndex = 0;
+            // 
+            // textBoxDest
+            // 
+            this.textBoxDest.Location = new System.Drawing.Point(101, 49);
+            this.textBoxDest.Name = "textBoxDest";
+            this.textBoxDest.Size = new System.Drawing.Size(187, 20);
+            this.textBoxDest.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Source Folder";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Dest Folder";
+            // 
+            // buttonBrowse1
+            // 
+            this.buttonBrowse1.Location = new System.Drawing.Point(294, 25);
+            this.buttonBrowse1.Name = "buttonBrowse1";
+            this.buttonBrowse1.Size = new System.Drawing.Size(32, 19);
+            this.buttonBrowse1.TabIndex = 4;
+            this.buttonBrowse1.Text = "...";
+            this.buttonBrowse1.UseVisualStyleBackColor = true;
+            this.buttonBrowse1.Click += new System.EventHandler(this.buttonBrowse1_Click);
+            // 
+            // buttonCopy
+            // 
+            this.buttonCopy.Location = new System.Drawing.Point(263, 172);
+            this.buttonCopy.Name = "buttonCopy";
+            this.buttonCopy.Size = new System.Drawing.Size(75, 23);
+            this.buttonCopy.TabIndex = 6;
+            this.buttonCopy.Text = "Copy";
+            this.buttonCopy.UseVisualStyleBackColor = true;
+            this.buttonCopy.Click += new System.EventHandler(this.buttonCopy_Click);
+            // 
+            // buttonBrowse2
+            // 
+            this.buttonBrowse2.Location = new System.Drawing.Point(294, 50);
+            this.buttonBrowse2.Name = "buttonBrowse2";
+            this.buttonBrowse2.Size = new System.Drawing.Size(32, 19);
+            this.buttonBrowse2.TabIndex = 7;
+            this.buttonBrowse2.Text = "...";
+            this.buttonBrowse2.UseVisualStyleBackColor = true;
+            this.buttonBrowse2.Click += new System.EventHandler(this.buttonBrowse2_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 208);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(371, 22);
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(39, 17);
+            this.toolStripStatusLabel1.Text = "Ready";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(200, 16);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(20, 172);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 9;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBoxSrc);
+            this.groupBox1.Controls.Add(this.textBoxDest);
+            this.groupBox1.Controls.Add(this.buttonBrowse2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.buttonBrowse1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(337, 90);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Copy settings";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkBoxVideo);
+            this.groupBox2.Controls.Add(this.checkBoxRaw);
+            this.groupBox2.Controls.Add(this.checkBoxJPG);
+            this.groupBox2.Location = new System.Drawing.Point(13, 108);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(336, 58);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "FileType";
+            // 
+            // checkBoxJPG
+            // 
+            this.checkBoxJPG.AutoSize = true;
+            this.checkBoxJPG.Location = new System.Drawing.Point(7, 20);
+            this.checkBoxJPG.Name = "checkBoxJPG";
+            this.checkBoxJPG.Size = new System.Drawing.Size(46, 17);
+            this.checkBoxJPG.TabIndex = 0;
+            this.checkBoxJPG.Text = "JPG";
+            this.checkBoxJPG.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxRaw
+            // 
+            this.checkBoxRaw.AutoSize = true;
+            this.checkBoxRaw.Location = new System.Drawing.Point(73, 20);
+            this.checkBoxRaw.Name = "checkBoxRaw";
+            this.checkBoxRaw.Size = new System.Drawing.Size(52, 17);
+            this.checkBoxRaw.TabIndex = 1;
+            this.checkBoxRaw.Text = "RAW";
+            this.checkBoxRaw.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxVideo
+            // 
+            this.checkBoxVideo.AutoSize = true;
+            this.checkBoxVideo.Location = new System.Drawing.Point(145, 20);
+            this.checkBoxVideo.Name = "checkBoxVideo";
+            this.checkBoxVideo.Size = new System.Drawing.Size(53, 17);
+            this.checkBoxVideo.TabIndex = 2;
+            this.checkBoxVideo.Text = "Video";
+            this.checkBoxVideo.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(371, 230);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonCopy);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Copy Images & RAW";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxSrc;
+        private System.Windows.Forms.TextBox textBoxDest;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonBrowse1;
+        private System.Windows.Forms.Button buttonCopy;
+        private System.Windows.Forms.Button buttonBrowse2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox checkBoxVideo;
+        private System.Windows.Forms.CheckBox checkBoxRaw;
+        private System.Windows.Forms.CheckBox checkBoxJPG;
     }
 }
 
--- Copy Images From SD/Form1.cs
+++ Copy Images From SD/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,214 @@ namespace Copy_Images_From_SD
 {
     public partial class Form1 : Form
     {
+        BackgroundWorker m_oWorker;
+        List<string> allowedCopy;
         public Form1()
         {
             InitializeComponent();
+            m_oWorker = new BackgroundWorker();
+
+            // Create a background worker thread that ReportsProgress &
+            // SupportsCancellation
+            // Hook up the appropriate events.
+            m_oWorker.DoWork += new DoWorkEventHandler(m_oWorker_DoWork);
+            m_oWorker.ProgressChanged += new ProgressChangedEventHandler
+                    (m_oWorker_ProgressChanged);
+            m_oWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler
+                    (m_oWorker_RunWorkerCompleted);
+            m_oWorker.WorkerReportsProgress = true;
+            m_oWorker.WorkerSupportsCancellation = true;
         }
+        #region Background worker
+
+        
+        /// <summary>
+        /// On completed do the appropriate task
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void m_oWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            // The background process is complete. We need to inspect
+            // our response to see if an error occurred, a cancel was
+            // requested or if we completed successfully.  
+            if (e.Cancelled)
+            {
+                toolStripStatusLabel1.Text = "Task Cancelled.";
+            }
+
+            // Check to see if an error occurred in the background process.
+
+            else if (e.Error != null)
+            {
+                toolStripStatusLabel1.Text = "Error while performing background operation.";
+            }
+            else
+            {
+                // Everything completed normally.
+                toolStripStatusLabel1.Text = "Task Completed...";
+            }
+
+            //Change the status of the buttons on the UI accordingly
+            buttonCopy.Enabled = true;
+            buttonCancel.Enabled = false;
+        }
+
+        /// <summary>
+        /// Notification is performed here to the progress bar
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void m_oWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+
+            // This function fires on the UI thread so it's safe to edit
+
+            // the UI control directly, no funny business with Control.Invoke :)
+
+            // Update the progressBar with the integer supplied to us from the
+
+            // ReportProgress() function.  
+
+            toolStripProgressBar1.Value = e.ProgressPercentage;
+            toolStripStatusLabel1.Text = "Processing......" + toolStripProgressBar1.Value.ToString() + "%";
+        }
+
+        /// <summary>
+        /// Time consuming operations go here </br>
+        /// i.e. Database operations,Reporting
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void m_oWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string[] allfiles = System.IO.Directory.GetFiles(textBoxSrc.Text, "*.*", System.IO.SearchOption.AllDirectories);
+            double precntFactor = (double)100 / allfiles.Length;
+            for (int i = 0; i < allfiles.Length; i++)
+            {
+                processSingleFile(allfiles, i);
+
+
+                //Progress reporting 
+                m_oWorker.ReportProgress(Convert.ToInt32(i * precntFactor));
+                if (m_oWorker.CancellationPending)
+                {
+                    // Set the e.Cancel flag so that the WorkerCompleted event
+                    // knows that the process was cancelled.
+                    e.Cancel = true;
+                    m_oWorker.ReportProgress(0);
+                    return;
+                }
+            }
+
+            //Report 100% completion on operation completed
+            m_oWorker.ReportProgress(100);
+        }
+
+
+        #endregion
+
+        #region Events
+        private void buttonBrowse1_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(textBoxSrc.Text))
+                folderBrowserDialog1.SelectedPath = textBoxSrc.Text;
+            folderBrowserDialog1.ShowDialog();
+            textBoxSrc.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void buttonBrowse2_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(textBoxDest.Text))
+                folderBrowserDialog1.SelectedPath = textBoxDest.Text;
+            folderBrowserDialog1.ShowDialog();
+            textBoxDest.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void buttonCopy_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(textBoxDest.Text) || !Directory.Exists(textBoxSrc.Text))
+            { //TODO: make alerts looks more pro
+                MessageBox.Show("Direcotories do not exist");
+                return;
+            }
+            if (!buildAllowedList())
+            {
+                MessageBox.Show("Please mark at least one type to copy");
+                return;
+            }
+              //TODO: add auto save settings  
+
+            buttonCopy.Enabled = false;
+            buttonCancel.Enabled = true;
+            m_oWorker.RunWorkerAsync();
+            
+        }
+
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            if (m_oWorker.IsBusy)
+            {
+
+                // Notify the worker thread that a cancel has been requested.
+
+                // The cancel will not actually happen until the thread in the
+
+                // DoWork checks the m_oWorker.CancellationPending flag. 
+
+                m_oWorker.CancelAsync();
+            }
+        }
+        #endregion
+
+        #region Methods
+
+        private void processSingleFile(string[] allfiles, int i)
+        {
+            FileInfo info = new FileInfo(allfiles[i]);
+            if (fileShouldBeCopy(info))
+            {
+                string tempPath = getFileDestPath(info);
+                if (!Directory.Exists(textBoxDest.Text + "\\" + tempPath))
+                    Directory.CreateDirectory(textBoxDest.Text + "\\" + tempPath);
+                File.Copy(info.FullName, textBoxDest.Text + "\\" + tempPath + "\\" + info.Name, true);
+            }
+        }
+
+        private bool fileShouldBeCopy(FileInfo info)
+        {
+            if (allowedCopy.Contains(info.Extension.ToLower()))
+                return true;
+
+            return false;               
+        }
+        private string getFileDestPath(FileInfo info)
+        {
+            DateTime lastModified = System.IO.File.GetLastWriteTime(info.FullName);
+            return  info.Extension.Replace(".","") + "\\" + lastModified.ToString("yyyy.MM.dd");
+
+        }
+
+        private bool buildAllowedList()
+        {
+            //TODO: add more file types 
+            allowedCopy = new List<string>();
+            if (!checkBoxJPG.Checked && !checkBoxRaw.Checked && !checkBoxVideo.Checked)
+                return false;
+            if (checkBoxJPG.Checked)
+                allowedCopy.Add(".jpg");
+            if (checkBoxRaw.Checked)
+            {
+                allowedCopy.Add(".arw");
+            }
+            if (checkBoxVideo.Checked)
+            {
+                allowedCopy.Add(".mp4");
+            }
+            return true;
+
+        }       
+        #endregion
     }
 }
